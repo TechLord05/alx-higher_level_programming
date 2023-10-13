6d58cab080203d20ad82c01c7e452dc580b3d4d7
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-10-13 15:09:44 -0700

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    return sum(set(num for num in my_list if isinstance(num, int)))#!/usr/bin/python3
+    return sum(set(num for num in my_list if isinstance(num, int)))
