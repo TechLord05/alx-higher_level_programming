6a38bfbcce82caa6d0c1375ac190fc3a58465c23
adeife331 <ifeoluwa.adebayo2003@gmail.com>
2024-01-11 05:46:50 +0100

    update

--- 0x13-javascript_objects_scopes_closures/0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-// Check if an argument is provided
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  // Print the first argument
-  console.log(process.argv[2]);
-}
